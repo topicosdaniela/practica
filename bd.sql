b3c3fa8f4114f25f96a46e3852860d35a35fc92a
topicosdaniela <51199645+topicosdaniela@users.noreply.github.com>
2019-05-31 05:28:30 -0500

    Add files via upload

---
+++ bd.sql
@@ -0,0 +1,6 @@
+"CREATE DATABASE lalala;"
+"USE lalala;"
+"CREATE TABLE lala(id int NOT NULL AUTO_INCREMENT,la1 int , la2 int, PRIMARY KEY (id));"
+"SOURCE C:\Users\Dann\Desktop\topicos;"
+"INSERT INTO lala ( la1 , la2 ) VALUES ('nombre',20);"
+"INSERT INTO lala ( la1 , la2 ) VALUES ('nombre',20);"
