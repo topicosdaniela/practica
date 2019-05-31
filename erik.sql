6c6a1992f2697bcb43a29b870b3ce72a1964e899
topicosdaniela <danyxzyta369@gmail.com>
2019-05-31 14:22:36 -0500

    practica

--- erik.sql
+++ erik.sql
@@ -14,30 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `huerta`
---
-
-DROP TABLE IF EXISTS `huerta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `huerta` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `jimenez` text,
-  `jimenes` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `huerta`
---
-
-LOCK TABLES `huerta` WRITE;
-/*!40000 ALTER TABLE `huerta` DISABLE KEYS */;
-/*!40000 ALTER TABLE `huerta` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -48,4 +24,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-31  7:47:32
+-- Dump completed on 2019-05-31 14:22:35
